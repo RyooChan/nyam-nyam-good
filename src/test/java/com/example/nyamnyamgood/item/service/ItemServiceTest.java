46d916b8a84f7f427af293d70f4e89ea2fe5231e
RyooChan <53744363+RyooChan@users.noreply.github.com>
2024-02-10 02:52:47 +0900

    Transactional

--- src/test/java/com/example/nyamnyamgood/item/service/ItemServiceTest.java
+++ src/test/java/com/example/nyamnyamgood/item/service/ItemServiceTest.java
@@ -19,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class ItemServiceTest {
 
     @Autowired
