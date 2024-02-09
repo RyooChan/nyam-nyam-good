5b0e81bb3a2c3e87e4570728e63093e046bb28d9
RyooChan <53744363+RyooChan@users.noreply.github.com>
2024-02-10 01:25:16 +0900

    entity 설정 변경

--- src/main/java/com/example/nyamnyamgood/customer/entity/Customer.java
+++ src/main/java/com/example/nyamnyamgood/customer/entity/Customer.java
@@ -1,10 +1,18 @@
 package com.example.nyamnyamgood.customer.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Builder
 public class Customer {
     @Id
--- src/main/java/com/example/nyamnyamgood/store/entity/Store.java
+++ src/main/java/com/example/nyamnyamgood/store/entity/Store.java
@@ -1,10 +1,17 @@
 package com.example.nyamnyamgood.store.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Builder
 public class Store {
     @Id
